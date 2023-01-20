825fea0e650b82166045da0781afc2b2c6a580cb
romipuntel <r.puntel@hotmail.com>
2023-01-20 13:00:06 -0500

    logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"></a>
+                <a className="navbar-brand" href="#">
+                    <img src="../logo.png" className="logo" alt="logo"></img></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
